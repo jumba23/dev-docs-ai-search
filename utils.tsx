3578843f4e1a208c451ebe890f5c74948e391749
Goran SD <g.cvetic23@gmail.com>
2023-09-30 00:25:29 -0700

    Removed unused docs

--- utils.tsx
+++ utils.tsx
@@ -22,7 +22,6 @@ export const queryPineconeVectorStoreAndQueryLLM = async ({
   console.log("Querying Pinecone vector store...");
   // 2. Retrieve the Pinecone index
   const index = client.index(indexName);
-  console.log(`Pinecone index retrieved: ${JSON.stringify(index)}`);
   if (!question) throw new Error("No question provided");
   // 3. Create query embedding
   const queryEmbedding = await new OpenAIEmbeddings().embedQuery(question);
@@ -74,11 +73,9 @@ export const createPineconeIndex = async ({
     console.log(`Creating "${indexName}"...`);
     // 5. Create index
     await client.createIndex({
-      createRequest: {
-        name: indexName,
-        dimension: vectorDimension,
-        metric: "cosine",
-      },
+      name: indexName,
+      dimension: vectorDimension,
+      metric: "cosine",
     });
     // 6. Log successful creation
     console.log(
@@ -104,7 +101,6 @@ export const updatePineconeIndex = async ({
   console.log(`Pinecone index retrieved: ${indexName}`);
   // 3. Process each document in the docs array
   for (const doc of docs) {
-    console.log(`Processing document: ${doc.metadata.source}`);
     const txtPath = doc.metadata.source;
     const text = doc.pageContent;
     // 4. Create RecursiveCharacterTextSplitter instance
@@ -114,7 +110,6 @@ export const updatePineconeIndex = async ({
     console.log("Splitting text into chunks...");
     // 5. Split text into chunks (documents)
     const chunks = await textSplitter.createDocuments([text]);
-    console.log(`Text split into  chunks : ${JSON.stringify(chunks)}`);
     console.log(
       `Calling OpenAI's Embedding endpoint documents with ${chunks.length} text chunks ...`
     );
@@ -142,14 +137,7 @@ export const updatePineconeIndex = async ({
         },
       };
       batch.push(vector);
-      console.log("batch", batch.length);
-      // check if batch is an array
-      if (!Array.isArray(batch)) {
-        throw new Error("batch is not an array");
-      }
 
-      console.log("batch raw", batch);
-      //print batch length
       // When batch is full or it's the last item, upsert the vectors
       if (batch.length === batchSize || idx === chunks.length - 1) {
         console.log("im upserting");
